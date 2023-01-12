1a1069fd62f19db79dc41c030e288b5f0660ad7d
Nihat.Yurtseven <yurtsevenihat@gmail.com>
2023-01-12 11:47:20 +0300

    fix

--- BorusanCaseAPI/Program.cs
+++ BorusanCaseAPI/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options => options.AddPolicy(name:"BorusanCaseOrigin",
     policy => {
-        policy.WithOrigins().AllowAnyMethod().AllowAnyHeader();
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
     }));
 
 var app = builder.Build();
